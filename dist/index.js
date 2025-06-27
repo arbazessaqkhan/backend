c5ba85f1574b3c473b006c0acd738f9cd90bab90
Arbaz Khan <arbazibnishaq@gmail.com>
2025-06-27 17:10:30 +0530

    ac

--- dist/index.js
+++ dist/index.js
@@ -922,14 +922,13 @@ import { createServer as createViteServer, createLogger } from "vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 var vite_config_default = defineConfig(async () => {
   const plugins = [
     react(),
     runtimeErrorOverlay()
   ];
   if (process.env.NODE_ENV !== "production" && process.env.REPL_ID !== void 0) {
-    const { cartographer } = await import("@replit/vite-plugin-cartographer");
+    const { cartographer } = await 
     plugins.push(cartographer());
   }
   return {
